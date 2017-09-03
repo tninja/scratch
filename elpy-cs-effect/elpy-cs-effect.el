2075dbcc385d92fed2c499e030c32f48548446f0
Kang Tu <tninja@Pengs-MacBook-Pro.local>
2017-09-02 17:12:50 -0700

    remove alda

--- elpy-cs-effect/elpy-cs-effect.el
+++ elpy-cs-effect/elpy-cs-effect.el
@@ -17,18 +17,7 @@
 (defun elpy-shell-send-region-or-buffer-with-alert ()
   (interactive)
   (lexical-let*
-	  (
-	   (score "
-violin \"violin-1\":
-  o4 f2   g4 a   b-2   a
-violin \"violin-2\":
-  o4 c2   e4 f   f2    f
-viola:
-  o3 a2 > c4 c   d2    c
-cello:
-  o3 f2   c4 f < b-2 > f
-")
-	   (msg-list '("Affirmative" "Roger that" "Acknowledge")))
+	  ((msg-list '("Affirmative" "Roger that" "Acknowledge")))
 	(osx-lib-say-rnd msg)
 	(alda-play-text score)
 	(elpy-shell-send-region-or-buffer)))
