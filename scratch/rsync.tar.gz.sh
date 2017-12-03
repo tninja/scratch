d33738cc45d1de5698ffb907c404d3e3cbe53d24
Kang Tu <tninja@Pengs-MacBook-Pro.local>
2017-12-02 22:01:30 -0800

    stage current stuff

---
+++ scratch/rsync.tar.gz.sh
@@ -0,0 +1,7 @@
+rsync --progress kangtu@172.20.10.3:/Users/kangtu/feldspar/trending.tar.gz .
+rsync --progress kangtu@172.20.10.3:/Users/kangtu/feldspar/hadoop.tar.gz .
+rsync --progress kangtu@172.20.10.3:/Users/kangtu/feldspar/fds_app.trending.tar.gz .
+rsync --progress kangtu@172.20.10.3:/Users/kangtu/feldspar/sys-reports.tar.gz .
+rsync --progress kangtu@172.20.10.3:/Users/kangtu/feldspar/feldspar-analytics-algorithm.tar.gz .
+rsync --progress kangtu@172.20.10.3:/Users/kangtu/feldspar/dataeng-docs.tar.gz .
+rsync --progress kangtu@172.20.10.3:/Users/kangtu/feldspar/scratch.tar.gz .
